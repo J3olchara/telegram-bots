d138b45773114ff21b0d7bd10a7df456b7cf1358
J3olchara <71733778+J3olchara@users.noreply.github.com>
2022-03-13 19:13:10 +0300

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -3,7 +3,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
-TOKEN = "5245666893:AAGwJqNQ0oR5_tMaiG4XgxWaNU0g-Ard0Pc"
+TOKEN = ""
 #BOT_URL = 'https://melodydotobot.herokuapp.com/'
 bot = Bot(token=(TOKEN))
 dp = Dispatcher(bot, storage=storage)
